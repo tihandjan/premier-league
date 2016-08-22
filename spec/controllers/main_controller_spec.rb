b93aefd56d1e894ac729e66136c886e4126811d7
Tikhonenko <tihandjan198921@gmail.com>
2016-08-22 01:14:01 +0000

    add empty footer and header partials

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe MainController, type: :controller do
-
+    it "renders index template" do
+       get :index
+       
+       expect(response).to render_template(:index)
+    end
 end
