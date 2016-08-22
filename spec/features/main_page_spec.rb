aa6e3788d917e610d1fca3d048ae4f0151883e9e
Tikhonenko <tihandjan198921@gmail.com>
2016-08-22 05:22:38 +0000

    add top nav and styling for it

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature "main page" do
     
-    scenario 'main page title' do
+    scenario 'main page have title' do
        visit '/'
        
        expect(page).to have_title('Английская премьер лига')
