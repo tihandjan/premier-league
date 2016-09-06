e4fb882f3432b299e3e909c7d136f241b0f1ca8c
Tikhonenko <tihandjan198921@gmail.com>
2016-09-06 18:40:02 +0000

    change some styling

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
     
     def index
-        @news = Article.order('created_at DESC').where("category = 'news'").first(4)
+        @news = Article.order('created_at DESC').where("category = 'news'").all
         @articles = Article.order('created_at DESC').where("category = 'article'").first(2)
     end
     
