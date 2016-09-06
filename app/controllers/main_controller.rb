079e5ddf350ec5348e8382c5261d075847f04ab1
Tikhonenko <tihandjan198921@gmail.com>
2016-09-06 17:21:34 +0000

    add functionality to add news/image resize

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
     
     def index
-        
+        @articles = Article.order('created_at DESC').where("category = 'news'").first(4)
     end
     
 end
