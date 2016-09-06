5bb1b2bb325267559482892bd11b3a15eec6c4e3
Tikhonenko <tihandjan198921@gmail.com>
2016-09-06 19:09:37 +0000

    validate article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
     has_many :pictures, dependent: :destroy
+    validates :title, presence: true, length: { minimum: 10, maximum: 150 }
+    validates :summary, presence: true, length: { minimum: 10, maximum: 250 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 10000 }
+    validates :picture, presence: true
     mount_uploader :picture, PictureUploader
     
     accepts_nested_attributes_for :pictures
