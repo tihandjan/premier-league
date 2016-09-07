dd2090c01e4e19f6207fd425a554858c4e7865ca
Tikhonenko <tihandjan198921@gmail.com>
2016-09-07 16:56:40 +0000

    devise

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
     has_many :pictures, dependent: :destroy
     validates :title, presence: true, length: { minimum: 10, maximum: 150 }
     validates :summary, presence: true, length: { minimum: 10, maximum: 250 }
-    validates :description, presence: true, length: { minimum: 10, maximum: 10000 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 5000 }
     validates :picture, presence: true
     mount_uploader :picture, PictureUploader
     
