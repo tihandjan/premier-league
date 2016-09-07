aa43716dd6f1486c3d76fc8515ba962e29ea90ab
Tikhonenko <tihandjan198921@gmail.com>
2016-09-07 16:56:40 +0000

    devise

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+     it { should belong_to(:article) } 
+  end
+  
+  describe "validation" do
+     it { should validate_length_of(:description).is_at_least(10) }
+     it { should validate_length_of(:description).is_at_most(5000) } 
+  end
 end
