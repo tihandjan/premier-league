5eb81df89b12b44de92f75bd76455b60045cbae6
Tikhonenko <tihandjan198921@gmail.com>
2016-09-09 12:52:59 +0000

    fix bug with email auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,25 +10,44 @@ class User < ActiveRecord::Base
   
   # facebook
   def self.find_for_facebook_oauth access_token
-    if user = User.where(:uid =>  access_token.uid, :provider => access_token.provider).first
-      user
-    else 
-      User.create!(:provider => access_token.provider, :uid =>  access_token.uid, :avatarc => access_token.info.image, :username => access_token.extra.raw_info.name, :email => access_token.extra.raw_info.email, :password => Devise.friendly_token[0,20]) 
+    
+    if self.where(email: access_token.info.email).exists?
+      user = self.where(email: access_token.info.email).first
+      user.provider = access_token.provider
+      user.uid = access_token.uid
+      user.avatarc = access_token.info.image
+    else
+      user = self.create do |user|
+         user.provider = access_token.provider
+         user.uid = access_token.uid
+         user.username = access_token.extra.raw_info.name
+         user.email = access_token.extra.raw_info.email
+         user.avatarc = access_token.info.image
+         user.password = Devise.friendly_token[0,20]
+      end
     end
+    user
   end
   
   # google
   def self.find_for_google_oauth auth
     
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    if self.where(email: auth.info.email).exists?
+      user = self.where(email: auth.info.email).first
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.info.email
       user.avatarc = auth.info.image
-      user.username = auth.extra.raw_info.name
-      user.password = Devise.friendly_token[0,20]
+    else
+      user = self.create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.avatarc = auth.info.image
+        user.username = auth.extra.raw_info.name
+        user.password = Devise.friendly_token[0,20]
+      end
     end
-    
+    user
   end
   
   
