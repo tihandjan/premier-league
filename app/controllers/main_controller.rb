001f77d00e5d0adc3f7a0f18873533f2a90b6262
Tikhonenko <tihandjan198921@gmail.com>
2016-09-10 17:23:28 +0000

    add show_page/

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,4 +5,9 @@ class MainController < ApplicationController
         @articles = Article.order('created_at DESC').where("category = 'article'").first(2)
     end
     
+    def show
+        @news = Article.order('created_at DESC').where("category = 'news'").all
+        @article = Article.find(params[:id])
+    end
+    
 end
