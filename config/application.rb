cce0d4c157ee8091e9df3e938c51e42d858f9f52
Tikhonenko <tihandjan198921@gmail.com>
2016-09-11 19:16:54 +0000

    fonts

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module Workspace
     config.i18n.default_locale = :ru
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('/app/assets/fonts')
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
