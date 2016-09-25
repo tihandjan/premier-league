7955247a20941fb9998314248ebc2c0b3b41755c
Tikhonenko <tihandjan198921@gmail.com>
2016-09-25 22:41:20 +0000

    my team ajax

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,18 @@
 module MainHelper
+    
+    def select_team
+        [
+        "Manchester City FC", 
+        "Chelsea FC", 
+        "Manchester United FC", 
+        "Everton FC", 
+        "Tottenham Hotspur FC", 
+        "Arsenal FC", 
+        "Leicester City FC", 
+        "Liverpool FC", 
+        "West Ham United FC", 
+        "Swansea City FC"
+        ].map{ |team| [team] }
+    end
+    
 end
