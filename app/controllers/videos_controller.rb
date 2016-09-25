344b7903bae20a22238148e8a327e7b9ad15a0cc
Tikhonenko <tihandjan198921@gmail.com>
2016-09-25 11:29:39 +0000

    tests for videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,7 +3,6 @@ class VideosController < ApplicationController
     def index
         @videos = Video.all
         @news = Article.order('created_at DESC').where("category = 'news'").first(20)
-        @articles = Article.order('created_at DESC').where("category = 'article'").first(4)
     end
     
     def show
