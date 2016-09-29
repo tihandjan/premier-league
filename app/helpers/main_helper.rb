7d0172f5337c2900f8a830e556e854bd1ffd23f4
Tikhonenko <tihandjan198921@gmail.com>
2016-09-29 15:27:00 +0000

    add select box team in russian

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -2,17 +2,27 @@ module MainHelper
     
     def select_team
         [
-        "Manchester City FC", 
-        "Chelsea FC", 
-        "Manchester United FC", 
-        "Everton FC", 
-        "Tottenham Hotspur FC", 
-        "Arsenal FC", 
-        "Leicester City FC", 
-        "Liverpool FC", 
-        "West Ham United FC", 
-        "Swansea City FC"
-        ].map{ |team| [team] }
+            ["Manchester United FC", "Ман Юн"],
+            ["Arsenal FC", "Арсенал"],
+            ["Chelsea FC", "Челси"],
+            ["Manchester City FC", "Ман Сити"],
+            ["Liverpool FC", "Ливерпуль"],
+            ["Tottenham Hotspur FC", "Тотенхем"],
+            ["Leicester City FC", "Лестер"],
+            ["Everton FC", "Эвертон"],
+            ["West Ham United FC", "Вест Хем"],
+            ["West Bromwich Albion FC", "Вест Бром"],
+            ["Sunderland AFC", "Сандерленд"],
+            ["Swansea City FC", "Свонси"],
+            ["Crystal Palace FC", "КПЛ"],
+            ["Southampton FC", "Саунгемптон"],
+            ["Watford FC", "Вотфорд"],
+            ["Burnley FC", "Бернли"],
+            ["Hull City FC", "Хал Сити"],
+            ["AFC Bournemouth", "Борнмут"],
+            ["Middlesbrough FC", "Мидлсбро"],
+            ["Stoke City FC", "Сток Сити"]
+        ].map{ |team_en, team_ru| [team_ru, team_en] }
     end
     
     def change_blank current_user
