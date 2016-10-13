5165f09eff0b6f1141d725d1511f4c9910adecaf
Tikhonenko <tihandjan198921@gmail.com>
2016-10-13 15:53:33 +0000

    setting Time zone

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module Workspace
     
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-
+    
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = :local
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
