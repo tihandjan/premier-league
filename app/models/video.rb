d34fcc640ea2338540a3960e3ca54e3fbcd0b3be
Tikhonenko <tihandjan198921@gmail.com>
2016-10-13 00:39:23 +0000

    create user friendly url

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
     mount_uploader :picture, PictureUploader
     validates :title, presence: true
     validates :code, presence: true
+    
+    def to_param
+      "#{id} #{title}".parameterize
+    end
 end
