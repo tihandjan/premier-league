f69a253f118aab1f6d2346f3bf1846821996a7be
Tikhonenko <tihandjan198921@gmail.com>
2016-10-16 21:39:35 +0000

    remove match model/create fixture model/ create update_fixtures method

--- db/migrate/20161010003634_add_column_to_match.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToMatch < ActiveRecord::Migration
-  def change
-    add_column :matches, :league, :string
-  end
-end
---
+++ spec/factories/fixtures.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :fixture do
+    league "MyString"
+    fixture "MyText"
+  end
+end
