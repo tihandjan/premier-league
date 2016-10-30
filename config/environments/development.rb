cc402160dc1ee4cd22a75e2a6286786db8c64d67
Tikhonenko <tihandjan198921@gmail.com>
2016-10-30 18:04:56 +0000

    fix bug on mobile main page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
-  host = '0.0.0.0:8080'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
 
   # Print deprecation notices to the Rails logger.
