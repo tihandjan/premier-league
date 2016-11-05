45739d0bb8e7de31bee6d056ff1e78f87de1af50
tihandjan <tihandjan198921@gmail.com>
2016-11-05 18:11:06 +0200

    add method link_to_nes_or_article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,16 @@
 class ArticlesController < ApplicationController
  
+  def index
+    @articles = Article.order('created_at DESC').where(category: 'news').all
+  end
+
+  def news
+    
+  end
+
   def show
-        @article = Article.find(params[:id])
-        @news = Article.order('created_at DESC').where("category = 'news'").all_except(@article).first(15)
+    @article = Article.find(params[:id])
+    @news = Article.order('created_at DESC').where(category: 'news').all_except(@article).first(15)
   end
     
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+
+  def link_to_news_or_article cat
+    if cat == 'article'
+      link_to 'Статьи', articles_path
+    else
+      link_to 'Новости', news_path
+    end
+  end
+
 end
