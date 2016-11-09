00961b2a25bfb15e337f8a0e3281455ee8754523
tihandjan <tihandjan198921@gmail.com>
2016-11-09 12:25:04 +0200

    change db_auto_posting

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -3,15 +3,19 @@ ActiveAdmin.register Video do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :title, :picture, :youtube_link, :code
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+permit_params :title, :picture, :youtube_link, :code, :league
+
+
+    form do |f|
+        
+        inputs 'videos' do
+          input :title
+          input :league, as: :select, collection: ['apl', 'seria-a', 'bundesliga', 'laliga', 'chempions-league']
+          # input :team, as: :select, collection: select_team 
+          input :picture, as: :file
+          input :code
+        end
+    end
 
 
 end
