d02b9da3b8d760e86e86083ac971095d3f9b5fe3
tihandjan <tihandjan198921@gmail.com>
2016-11-12 17:50:17 +0200

    fix bug article-show/add active button to article_inex

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -8,4 +8,20 @@ module ArticlesHelper
     end
   end
 
+  def index_article_active(active=nil, thisone=nil)
+    if active == 'apl-active' && thisone == 'apl'
+      'apl-active'
+    elsif active == 'laliga-active' && thisone == 'laliga'
+      'laliga-active'
+    elsif active == 'bundesliga-active' && thisone == 'bundesliga'
+        'bundesliga-active'  
+    elsif active == 'seria-a-active' && thisone == 'seria-a'
+        'seria-a-active'  
+    elsif active == 'chempions-league-active' && thisone == 'chempions-league'
+        'chempions-league-active'  
+    elsif active == 'rest-active' && thisone == 'rest'
+        'rest-active'
+    end    
+  end
+
 end
