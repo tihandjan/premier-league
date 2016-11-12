cb106103101ea61b74d4ff87185a5e78d30e6d82
tihandjan <tihandjan198921@gmail.com>
2016-11-12 19:06:51 +0200

    add active button to video_index

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -11,23 +11,29 @@ class VideosController < ApplicationController
 
         if params[:league_id] == 'apl'
           @h1 = "Привет я буду h1 для #{params[:league_id]}"
+          @active = 'apl-active'
         elsif params[:league_id] == 'seria-a'
           @h1 = "Привет я буду h1 для #{params[:league_id]}"
+          @active = 'seria-a-active'
         elsif params[:league_id] == 'bundesliga'
           @h1 = "Привет я буду h1 для #{params[:league_id]}"
+          @active = 'bundesliga-active'
         elsif params[:league_id] == 'laliga'
           @h1 = "Привет я буду h1 для #{params[:league_id]}"
+          @active = 'laliga-active'
         elsif params[:league_id] == 'chempions-league'
           @h1 = "Привет я буду h1 для #{params[:league_id]}"
+          @active = 'chempions-league-active'
         else
           @h1 = "Привет я буду h1 для видео"
+          @active = 'rest-active'
         end
     end
-    
+     
     def show
         @editional_class = 'articles'
-        @video = Video.find(params[:league], params[:id])
-        @videos = Video.order('created_at DESC').first(4)
+        @video = Video.find(params[:id])
+        @videos = Video.order('created_at DESC').all_except(@video).first(4)
         @news = Article.order('created_at DESC').where("category = 'news'").first(12)
     end
     
