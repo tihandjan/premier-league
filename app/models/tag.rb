da77e6f31f8bcd46df00f193db16b7851ccc4d59
tihandjan <tihandjan198921@gmail.com>
2016-11-19 14:05:32 +0200

    tags for articles are finished

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,8 @@ class Tag < ActiveRecord::Base
     has_many :articles, through: :taggings
     has_many :videos, through: :taggings
     validates :name, uniqueness: true
+
+    def to_param
+    "#{id} #{name}".parameterize
+  end
 end
