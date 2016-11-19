5e9c777773576caf9fdff167dda5d2195dec4800
tihandjan <tihandjan198921@gmail.com>
2016-11-19 14:05:32 +0200

    tags for articles are finished

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,9 @@ class TagsController < ApplicationController
     end
 
     def show
-        @articles = Tag.find_by(name: params[:id]).articles
+        @h1 = params[:id]
+        @articles = Tag.find(params[:id]).articles.order('created_at DESC').paginate(page: params[:page], per_page: 8)
+        @news = Article.order('created_at DESC').first(14)
+        @editional_class = 'articles'
     end
 end
