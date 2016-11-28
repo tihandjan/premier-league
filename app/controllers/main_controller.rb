28ff19a4964af523e0008aaa1a89338db82e5a86
tihandjan <tihandjan198921@gmail.com>
2016-11-29 01:34:47 +0200

    rewrite table functionality/refactoring

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,8 +4,10 @@ class MainController < ApplicationController
         @news = Article.order('created_at DESC').where("category = 'news'").first(20)
         @articles = Article.order('created_at DESC').where("category = 'article'").first(4)
         @videos = Video.order('created_at DESC').first(5)
-        update_fixtures
-        choose_league_table
+        if Delayed::Job.count == 0
+            Table.delay.set_table_data
+        end
+        @table = Table.all
     end
     
     def change_my_team
@@ -18,79 +20,11 @@ class MainController < ApplicationController
 
     def policy
     end
-
-    def choose_league_table
-        if Table.count != 5 || (Time.current - Table.last.created_at)/60 > 60 && (HTTParty.get("http://api.football-data.org/v1/competitions/426/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})).code == 200
-           
-            Table.destroy_all
-            en = HTTParty.get("http://api.football-data.org/v1/competitions/426/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            pl_sp = HTTParty.get("http://api.football-data.org/v1/competitions/436/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            seria_a = HTTParty.get("http://api.football-data.org/v1/competitions/438/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            bundes_liga = HTTParty.get("http://api.football-data.org/v1/competitions/430/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            chemp_liga = HTTParty.get("http://api.football-data.org/v1/competitions/440/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            
-            Table.create!(league: 'germany', data: bundes_liga)
-            Table.create!(league: 'cl', data: chemp_liga)
-            Table.create!(league: 'italy', data: seria_a)
-            Table.create!(league: 'spain', data: pl_sp)
-            Table.create!(league: 'england', data: en)
-            
-            
-            @response_table_bundes_liga = eval(Table.find_by(league: 'germany').data)
-            @response_table_chemp_liga = eval(Table.find_by(league: 'cl').data)
-            @response_table_seria_a = eval(Table.find_by(league: 'italy').data)
-            @response_table_pl_sp = eval(Table.find_by(league: 'spain').data)
-            @response_table = eval(Table.find_by(league: 'england').data)
-            
-        else
-            
-            @response_table_bundes_liga = eval(Table.find_by(league: 'germany').data)
-            @response_table_chemp_liga = eval(Table.find_by(league: 'cl').data)
-            @response_table_seria_a = eval(Table.find_by(league: 'italy').data)
-            @response_table_pl_sp = eval(Table.find_by(league: 'spain').data)
-            @response_table = eval(Table.find_by(league: 'england').data)
-            
-        end
-    end
     
     private
     
     def user_params
         params.require(:user).permit(:team)    
-    end
-    
-    def update_fixtures
-        if Fixture.count != 5 || (Time.current - Fixture.last.created_at)/60 > 10 && (HTTParty.get("http://api.football-data.org/v1/competitions/426/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})).code == 200
-
-                
-            Fixture.destroy_all
-            en = HTTParty.get("http://api.football-data.org/v1/competitions/426/fixtures", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            pl_sp = HTTParty.get("http://api.football-data.org/v1/competitions/436/fixtures", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            seria_a = HTTParty.get("http://api.football-data.org/v1/competitions/438/fixtures", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            bundes_liga = HTTParty.get("http://api.football-data.org/v1/competitions/430/fixtures", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            chemp_liga = HTTParty.get("http://api.football-data.org/v1/competitions/440/fixtures", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
-            
-            Fixture.create!(league: 'germany', fixture: bundes_liga)
-            Fixture.create!(league: 'cl', fixture: chemp_liga)
-            Fixture.create!(league: 'italy', fixture: seria_a)
-            Fixture.create!(league: 'spain', fixture: pl_sp)
-            Fixture.create!(league: 'england', fixture: en)
-            
-            @response_bundes_liga = eval(Fixture.find_by(league: 'germany').fixture)
-            @response_chemp_liga = eval(Fixture.find_by(league: 'cl').fixture)
-            @response_seria_a = eval(Fixture.find_by(league: 'italy').fixture)
-            @response_pl_sp = eval(Fixture.find_by(league: 'spain').fixture)
-            @response = eval(Fixture.find_by(league: 'england').fixture)
-                
-            
-        else
-            @response_bundes_liga = eval(Fixture.find_by(league: 'germany').fixture)
-            @response_chemp_liga = eval(Fixture.find_by(league: 'cl').fixture)
-            @response_seria_a = eval(Fixture.find_by(league: 'italy').fixture)
-            @response_pl_sp = eval(Fixture.find_by(league: 'spain').fixture)
-            @response = eval(Fixture.find_by(league: 'england').fixture)
-        end
-    end
-    
+    end 
     
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,57 @@
 class Table < ActiveRecord::Base
+
+    def self.set_table_data
+      if self.count < 10 || (Time.current - self.last.created_at)/60 > 60 && (HTTParty.get("http://api.football-data.org/v1/competitions/426/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})).code == 200
+         
+          
+          @en = HTTParty.get("http://api.football-data.org/v1/competitions/426/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
+          @pl_sp = HTTParty.get("http://api.football-data.org/v1/competitions/436/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
+          @seria_a = HTTParty.get("http://api.football-data.org/v1/competitions/438/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
+          @bundes_liga = HTTParty.get("http://api.football-data.org/v1/competitions/430/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
+          @chemp_liga = HTTParty.get("http://api.football-data.org/v1/competitions/440/leagueTable", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
+          
+          self.transaction do
+            @data = []
+            @en['standing'].each do |data|
+                @data << self.new(league: 'apl', position: data['position'], teamName: data['teamName'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], wins: data['wins'], draws: data['draws'], losses: data['losses'])
+            end
+            @pl_sp['standing'].each do |data|
+                @data << self.new(league: 'laliga', position: data['position'], teamName: data['teamName'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], wins: data['wins'], draws: data['draws'], losses: data['losses'])
+            end
+            @seria_a['standing'].each do |data|
+                @data << self.new(league: 'seria-a', position: data['position'], teamName: data['teamName'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], wins: data['wins'], draws: data['draws'], losses: data['losses'])
+            end
+            @bundes_liga['standing'].each do |data|
+                @data << self.new(league: 'bundesliga', position: data['position'], teamName: data['teamName'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], wins: data['wins'], draws: data['draws'], losses: data['losses'])
+            end
+            @chemp_liga['standings']['A'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            @chemp_liga['standings']['B'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            @chemp_liga['standings']['C'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            @chemp_liga['standings']['D'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            @chemp_liga['standings']['E'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            @chemp_liga['standings']['F'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            @chemp_liga['standings']['G'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            @chemp_liga['standings']['H'].each do |data|
+                @data << self.new(league: 'chempions-league', position: data['rank'], teamName: data['team'], crestURI: data['crestURI'], playedGames: data['playedGames'], points: data['points'], goals: data['goals'], goalsAgainst: data['goalsAgainst'], goalDifference: data['goalDifference'], group: data['group'])
+            end
+            self.import @data, validate: false
+          end
+          where(['created_at < ?', Table.last.created_at]).delete_all
+      end
+  end
+
 end
