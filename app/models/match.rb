8bcf5221dd345c922b54152365837c2369850774
tihandjan <tihandjan198921@gmail.com>
2016-11-28 19:34:33 +0200

    activerecord-import and set_games method

--- app/models/match.rb
+++ app/models/match.rb
@@ -8,20 +8,25 @@ class Match < ActiveRecord::Base
         @response_ge = HTTParty.get("http://api.football-data.org/v1/competitions/430/fixtures", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
         @response_cl = HTTParty.get("http://api.football-data.org/v1/competitions/440/fixtures", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
         
-        @response_en['fixtures'].each do |data|
-            Match.create!(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'apl') 
-        end
-        @response_sp['fixtures'].each do |data|
-            Match.create!(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'laliga') 
-        end
-        @response_it['fixtures'].each do |data|
-            Match.create!(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'seria-a')
-        end
-        @response_ge['fixtures'].each do |data|
-            Match.create!(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'bundesliga')
-        end
-        @response_cl['fixtures'].each do |data|
-            Match.create!(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'chempions-league')
+        Match.transaction do
+            @data = []
+            @response_en['fixtures'].each do |data|
+                @data << Match.new(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'apl') 
+            end
+            @response_sp['fixtures'].each do |data|
+                @data << Match.new(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'laliga') 
+            end
+            @response_it['fixtures'].each do |data|
+                @data << Match.new(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'seria-a')
+            end
+            @response_ge['fixtures'].each do |data|
+                @data << Match.new(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'bundesliga')
+            end
+            @response_cl['fixtures'].each do |data|
+                @data << Match.new(homeTeamName: if_nul(data['homeTeamName']),awayTeamName: if_nul(data['awayTeamName']),date: if_nul(data['date']),status: if_nul(data['status']),selfFixtures: if_nul(data['_links']['self']['href']),matchday: if_nul(data['matchday']),goalsHomeTeam: if_nul(data['result']['goalsHomeTeam']),goalsAwayTeam: if_nul(data['result']['goalsAwayTeam']), league: 'chempions-league')
+            end
+
+            Match.import @data, validate: false
         end
     end
 
