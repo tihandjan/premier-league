d7dbf4f1090efef51ec559e29e897f3896b7bafa
tihandjan <tihandjan198921@gmail.com>
2016-12-02 03:05:40 +0200

    fix bug pg

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -30,7 +30,7 @@ class MainController < ApplicationController
     end
     
     def change_my_team
-        cookies[:user_team] = { value: params[:team], expires: 3.month.from_now, :domain => :all }
+        cookies[:user_team] = params[:team] 
         respond_to do |format|
            format.html { redirect_to root_path }
            format.js
