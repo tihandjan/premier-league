e15385d3c6467843c8298b8cf0049c775e6ccd6c
tihandjan <tihandjan198921@gmail.com>
2016-12-02 22:55:52 +0200

    sceleton for team/player models

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,69 +2,79 @@ class Team < ActiveRecord::Base
     has_many :players, dependent: :destroy
     
     def self.set_teams_premier_league
-        where(league: 'en').destroy_all
+        where(league: 'apl').destroy_all
         @response = HTTParty.get("http://api.football-data.org/v1/competitions/426/teams", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
         
         @response['teams'].each do |data|
-            Team.create!(league: 'en',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
+            self.create!(league: 'apl',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
             @players = HTTParty.get(data['_links']['players']['href'], :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
             @players['players'].each do |player|
-                Team.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
+                self.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
             end
         end
     end
     
     def self.set_teams_spanish_league
-        where(league: 'sp').destroy_all
+        where(league: 'laliga').destroy_all
         @response = HTTParty.get("http://api.football-data.org/v1/competitions/436/teams", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
         
         @response['teams'].each do |data|
-            Team.create!(league: 'sp',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
+            self.create!(league: 'laliga',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
             @players = HTTParty.get(data['_links']['players']['href'], :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
             @players['players'].each do |player|
-                Team.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
+                self.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
             end
         end
     end
     
     def self.set_teams_seria_a
-        where(league: 'it').destroy_all
+        where(league: 'seria-a').destroy_all
         @response = HTTParty.get("http://api.football-data.org/v1/competitions/438/teams", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
         
         @response['teams'].each do |data|
-            Team.create!(league: 'it',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
+            self.create!(league: 'seria-a',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
             @players = HTTParty.get(data['_links']['players']['href'], :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
             @players['players'].each do |player|
-                Team.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
+                self.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
             end
         end
     end
     
     def self.set_teams_bundes_league
-        where(league: 'ge').destroy_all
+        where(league: 'bundesliga').destroy_all
         @response = HTTParty.get("http://api.football-data.org/v1/competitions/430/teams", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
         
         @response['teams'].each do |data|
-            Team.create!(league: 'ge',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
+            self.create!(league: 'bundesliga',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
             @players = HTTParty.get(data['_links']['players']['href'], :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
             @players['players'].each do |player|
-                Team.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
+                self.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
             end
         end
     end
     
     def self.set_teams_champions_league
-        where(league: 'lc').destroy_all
+        where(league: 'chempions-league').destroy_all
         @response = HTTParty.get("http://api.football-data.org/v1/competitions/440/teams", :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
         
         @response['teams'].each do |data|
-            Team.create!(league: 'lc',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
+            self.create!(league: 'chempions-league',name: check_value(data['name']),code: check_value(data['code']),short_name: check_value(data['shortName']),market_value: check_value(data['squadMarketValue']),fixtures: check_value(data['_links']['fixtures']['href']),icon_url: check_value(data['crestUrl']))
             @players = HTTParty.get(data['_links']['players']['href'], :headers =>{"X-Auth-Token" => '568ace863a0348b896cc43d897338062'})
             @players['players'].each do |player|
-                Team.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
+                self.last.players.create(name: check_value(player['name']), position: check_value(player['position']), number: check_value(player['jerseyNumber']), date_of_birth: check_value(player['dateOfBirth']), nationality: check_value(player['nationality']), contract_until: check_value(player['contractUntil']), market_value: check_value(player['marketValue']))
             end
         end
     end
+
+    def self.set_teams_and_players
+        Team.set_teams_spanish_league
+        Team.set_teams_champions_league
+        Team.set_teams_premier_league
+        Team.set_teams_bundes_league
+        Team.set_teams_seria_a
+    end
+
+
     
     def self.check_value value
         if value.blank?
