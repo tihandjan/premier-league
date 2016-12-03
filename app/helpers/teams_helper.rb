17a6800a6bf69bd170cf77820dc164727b22d3a2
tihandjan <tihandjan198921@gmail.com>
2016-12-03 18:27:45 +0200

    team right block is done

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -15,4 +15,23 @@ module TeamsHelper
         return league
     end
 
+    def player_position player, pos
+        if (["Keeper"].include? player.position) && (pos == 'вратари')
+            true
+        elsif (["Central Midfield", "Left Midfield", "Midfield", "Mittelfeld", "Right Midfield"].include? player.position) && (pos == 'полузащита')
+            true
+        elsif (["Centre Back", "Right-Back", "Left-Back", "Defensive Midfield"].include? player.position) && (pos == 'защита')
+            true
+        elsif (["Secondary Striker", "Attacking Midfield", "Centre Forward", "Left Wing", "Right Wing",].include? player.position) && (pos == 'нападение')
+            true
+        end
+    end
+
+    def age(birth)
+        today = Date.today
+        age = today.year - birth.year
+        age -= 1 if birth.strftime("%m%d").to_i > today.strftime("%m%d").to_i
+        age
+    end
+
 end
